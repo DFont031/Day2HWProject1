713f5ead8d42280a5ddeee14813cf5bd01b66e1d
denni <denni@DennissePC>
2023-04-09 21:09:29 -0400

    Initial Commit...

---
+++ src/homework/HWClass1.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class HWClass1 {
+	public static void main(String[] args) {
+		
+		System.out.println("First line...");
+		
+	}
+	
+	public void display() {
+		
+		System.out.println("Pull example");
+	}
+
+}
